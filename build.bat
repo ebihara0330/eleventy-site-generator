d91f3491c626ebf4ad3155da5fc08313643b861e
ebihara0330 <128446355+ebihara0330@users.noreply.github.com>
2023-04-03 19:09:11 +0900

    Update and rename build.bat to build.sh

--- build.bat
+++
@@ -1,6 +0,0 @@
-cd /d %~dp0
-
-copy ".\data\metaData.js" ".\template\_data\metaData.js"
-copy ".\data\attachment\*" ".\template\attachment\"
-
-
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+cp "./data/metaData.js" "./template/_data/metaData.js"
+cp -r "./data/attachment/"* "./template/attachment/"
